c6b0ddb4c380388289b3dfb44b6e2f4182eed061
liujiakuoyx <627083017@qq.com>
2019-02-08 17:43:51 +0800

    delete:删除无用代码

--- src/main/java/com/liujiakuo/boss/controller/JobController.java
+++ src/main/java/com/liujiakuo/boss/controller/JobController.java
@@ -5,7 +5,6 @@ import com.liujiakuo.boss.base.http.PageDataResponse;
 import com.liujiakuo.boss.dao.job.Job;
 import com.liujiakuo.boss.dao.job.JobServiceImp;
 import com.liujiakuo.boss.utils.DataUtils;
-import jdk.nashorn.internal.scripts.JO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
