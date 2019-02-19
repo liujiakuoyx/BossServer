0f16f0f907f2bbb13d549573367f377f1201e4da
liujiakuo <liujiakuo@corp.netease.com>
2019-02-19 10:31:20 +0800

    添加打包插件、tomcat部署配置

--- src/main/java/com/liujiakuo/boss/BossApplication.java
+++ src/main/java/com/liujiakuo/boss/BossApplication.java
@@ -2,13 +2,19 @@ package com.liujiakuo.boss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BossApplication {
+public class BossApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BossApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BossApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BossApplication.class);
+    }
 }
 
