b5730e316c6b2fd66c737470a22f3875aa12842e
liujiakuo <liujiakuo@corp.netease.com>
2019-02-26 15:43:17 +0800

    加密数据库配置

--- src/main/java/com/liujiakuo/boss/BossApplication.java
+++ src/main/java/com/liujiakuo/boss/BossApplication.java
@@ -1,11 +1,13 @@
 package com.liujiakuo.boss;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableEncryptableProperties//启用数据库加密
 public class BossApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
